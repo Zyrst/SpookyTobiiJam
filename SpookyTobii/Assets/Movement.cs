3a63c90e55b9867aebd61a671c3f2c3085fe9927
Zyrst <zajreg@gmail.com>
2015-03-28 09:46:36 +0100

    Gårdagens arbete

--- SpookyTobii/Assets/Movement.cs
+++ SpookyTobii/Assets/Movement.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-
+	public float speed = 10.0f;
+	public float rotationSpeed = 10.0f;
+	public float jumpSpeed = 10.0f;
+	public float gravity = 20.0f;
+	private Vector3 movement = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,20 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		CharacterController controller = GetComponent<CharacterController>();
+		if(controller.isGrounded)
+		{
+			movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			movement = transform.TransformDirection(movement);
+			movement *= speed;
+			if(Input.GetButton("Jump"))
+			{
+				movement.y = jumpSpeed;
+			}
+		}
+			movement.y -= gravity * Time.deltaTime;
+			controller.Move(movement * Time.deltaTime);
+
 	}
 }
