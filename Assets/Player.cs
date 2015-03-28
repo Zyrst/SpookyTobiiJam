b6ff921777cee65a7c452731a6abcd98b99480bc
Zyrst <zajreg@gmail.com>
2015-03-28 22:45:47 +0100

    woh?

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public static Player instance  { get; private set; }
+	// Use this for initialization
+	void Awake () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
