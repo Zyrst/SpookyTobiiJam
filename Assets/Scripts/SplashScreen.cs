14e9b1bbc1338cc5ecd64ba93a7805460aa425b2
Ryxali <ryxali@gmail.com>
2015-03-29 03:39:53 +0200

    foo

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class SplashScreen : MonoBehaviour {
+	public static SplashScreen instance { get; private set; }
+	private Image splash;
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Debug.LogWarning("Multiple SplashScreen instances!");
+		}
+
+		splash = GetComponent<Image> ();
+	}
+
+	public IEnumerator fadeIn(Color color, float duration) {
+
+	}
+}
